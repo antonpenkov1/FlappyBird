5a4719b650beb58172e6d55e097864e3b7b5f33d
anton_penkov <anton.penkov1@gmail.com>
2023-11-06 18:07:52 +0100

    Initial Commit

---
+++ FlappyBird/FlappyBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyBirdApp.swift
+//  FlappyBird
+//
+//  Created by Антон Пеньков on 06.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlappyBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
